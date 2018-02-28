1c4d95cc7a07d274d77bbd422983d243f8159d33
Duy <duybck60t@gmail.com>
2018-02-28 16:53:43 +0700

    xx

---
+++ WebDemo/WebDemo/scripts/Students/Add.js
@@ -0,0 +1,26 @@
+﻿function Add()
+{
+    var rqData =
+    {
+        //"Name": document.getElementById("ip_name").value,
+        "MSV": document.getElementById("ip_MSV").value,
+        "birthday": document.getElementById("ip_birthday").value,
+        "MK": document.getElementById("ip_MK").value
+    }
+    Data.Name = $("#ip_name").val(); 
+    var a = 0;
+    $.ajax
+    ({
+        url: 'Student/jsonSaveStuden',
+        type: 'POST',
+        Data: JSON.stringify(rpData),
+        contentType: "application/json; charset=utf-8",
+        headers: {
+            "Content-Type": "application/json",
+        }
+        }).done(function(rs)
+        {
+            alert(rs.mes);
+        }                   
+        )
+}
